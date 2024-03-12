dbb19e24c1c94739849dac48d80eeaaf59b43c58
Your Name <you@example.com>
2024-03-12 18:42:05 +0300

    Settings for db on server

--- orders/models.py
+++ orders/models.py
@@ -24,8 +24,8 @@ class OrderitemQueryset(models.QuerySet):
 class Order(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.SET_DEFAULT, blank=True, null=True, verbose_name="Пользователь",
                              default='admin')
-    created_timestamp = models.DateTimeField(default=datetime.now, verbose_name="Дата создания заказа")
-    delivery_datetime = models.DateTimeField(null=True, default=datetime.now, verbose_name="Дата-время доставки")
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания заказа")
+    delivery_datetime = models.DateTimeField(null=True, verbose_name="Дата-время доставки")
     phone_number = PhoneNumberField(blank=True, verbose_name="Номер телефона")
     requires_delivery = models.BooleanField(default=False, verbose_name="Требуется доставка")
     delivery_address = models.TextField(null=True, blank=True, verbose_name="Адрес доставки")
