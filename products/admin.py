9efcfd9e4bfd2cf5003a7982d3425bd5592ba328
Your Name <you@example.com>
2024-03-13 16:49:09 +0300

    Settings for db on server

--- products/admin.py
+++ products/admin.py
@@ -7,7 +7,7 @@ from products.models import ProductCategory, Product, Basket
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'weight', 'category', 'on_or_off'  )
+    list_display = ('name', 'price', 'weight', 'category', 'on_or_off',)
     search_fields = ('name',)
     ordering = ('name',)
     prepopulated_fields = {"slug": ("name", )}
