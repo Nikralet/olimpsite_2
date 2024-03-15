1827df6f896270f22f2bc1670fc17c93abf70593
Your Name <you@example.com>
2024-03-15 12:40:39 +0300

    Settings for db on server

--- olimpsite/settings.py
+++ olimpsite/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = (
 )
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'static' / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
