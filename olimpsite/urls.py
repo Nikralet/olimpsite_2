f6fef825a31e76d79a8b4d8a4b81162c7cb30051
Your Name <you@example.com>
2024-03-15 20:01:50 +0300

    Settings for db on server

--- olimpsite/urls.py
+++ olimpsite/urls.py
@@ -36,7 +36,6 @@ urlpatterns = [
 if settings.DEBUG == True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
 if settings.DEBUG == False:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
