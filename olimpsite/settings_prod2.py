efdaa8857d59a34b8c7f57185fbcfd8cfd6249ca
Your Name <you@example.com>
2024-03-15 19:57:11 +0300

    Settings for db on server

---
+++ olimpsite/settings_prod2.py
@@ -0,0 +1,13 @@
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db1',
+        'USER': 'admin',
+        'PASSWORD': 'Judo-manga-top-123',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
