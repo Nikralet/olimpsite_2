227e2331420c9940d5ba21bac30ef2861846f1ca
Your Name <you@example.com>
2024-03-16 00:19:34 +0300

    Settings for db on server

--- olimpsite/settings.py
+++ olimpsite/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static", "static_dev"),
@@ -133,7 +133,7 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_prod")
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 
