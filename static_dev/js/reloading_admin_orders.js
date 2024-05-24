e1fccbf02d326c1347213820bcf689a2f1ff33cf
Your Name <you@example.com>
2024-05-24 23:21:27 +0300

    Settings for db on server

--- static_dev/js/reloading_admin_orders.js
+++ static_dev/js/reloading_admin_orders.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
 if (localStorage.length === 2) {
+    console.log('тут0')
     localStorage.setItem('reload', 'stopped');
 };
-if (localStorage.getItem('reload') == 'stopped') {
+console.log(localStorage.getItem('reload'))
+console.log(localStorage.length)
+if (localStorage.getItem('reload') === 'stopped') {
         alert('Обновление страницы выключено');
+        console.log('тут3')
     }
-if (localStorage.getItem('reload') == 'started') {
+if (localStorage.getItem('reload') === 'started') {
+            console.log('тут4')
             setTimeout(function () {
             location.reload()
             }, 6000);
@@ -14,12 +19,14 @@ if (localStorage.getItem('reload') == 'started') {
 myFunction = document.getElementById('reload');
 
         myFunction.addEventListener('click', function() {
-
-            if (localStorage.getItem('reload') == 'stopped'){
+            console.log('тут')
+            if (localStorage.getItem('reload') === 'stopped'){
                 localStorage.setItem('reload', 'started');
+                console.log('тут1')
             }
-            else if (localStorage.getItem('reload') == 'started'){
+            else if (localStorage.getItem('reload') === 'started'){
                 localStorage.setItem('reload', 'stopped');
+                console.log('тут2')
             }
         });
 });
