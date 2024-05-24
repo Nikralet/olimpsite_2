ff3496ccd5461820ca91abd12c04d0e4fb0dcbf5
Your Name <you@example.com>
2024-05-24 23:24:45 +0300

    Settings for db on server

--- static_dev/js/reloading_admin_orders.js
+++ static_dev/js/reloading_admin_orders.js
@@ -1,16 +1,13 @@
 $(document).ready(function () {
 if (localStorage.length === 1) {
-    console.log('тут0')
     localStorage.setItem('reload', 'stopped');
 };
 console.log(localStorage.getItem('reload'))
 console.log(localStorage.length)
 if (localStorage.getItem('reload') === 'stopped') {
         alert('Обновление страницы выключено');
-        console.log('тут3')
     }
 if (localStorage.getItem('reload') === 'started') {
-            console.log('тут4')
             setTimeout(function () {
             location.reload()
             }, 6000);
@@ -19,14 +16,11 @@ if (localStorage.getItem('reload') === 'started') {
 myFunction = document.getElementById('reload');
 
         myFunction.addEventListener('click', function() {
-            console.log('тут')
             if (localStorage.getItem('reload') === 'stopped'){
                 localStorage.setItem('reload', 'started');
-                console.log('тут1')
             }
             else if (localStorage.getItem('reload') === 'started'){
                 localStorage.setItem('reload', 'stopped');
-                console.log('тут2')
             }
         });
 });
